7fe44f454305cb434dd991d5b6b76f4b74f85ad0
dream <932370912@qq.com>
2021-02-06 13:06:51 +0800

    first commit

---
+++ src/main/java/com/roncoo/eshop/datasync/EshopDataSyncServiceApplication.java
@@ -0,0 +1,34 @@
+package com.roncoo.eshop.datasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * 数据同步服务
+ * @author dream
+ *
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class EshopDataSyncServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EshopDataSyncServiceApplication.class, args); 
+	}
+	
+	@Bean
+	public JedisPool jedisPool() {
+		JedisPoolConfig config = new JedisPoolConfig();
+		config.setMaxTotal(100);
+		config.setMaxIdle(5);
+		config.setMaxWaitMillis(1000 * 10); 
+		config.setTestOnBorrow(true);
+		return new JedisPool(config, "192.168.25.60", 1111);
+	}
+}
